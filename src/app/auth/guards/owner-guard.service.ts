91dbde605ea9721f110af7629c036b4a026355eb
Olaf Krawczyk <radeko94@gmail.com>
2017-10-22 01:43:41 +0200

    dashboard fixes

--- src/app/auth/guards/owner-guard.service.ts
+++ src/app/auth/guards/owner-guard.service.ts
@@ -2,13 +2,17 @@ import { AuthService } from './../auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class OwnerGuard implements CanActivate {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private rotuer: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
         console.log(this.authService.isOwner());
+        if (!this.authService.getUser()) {
+            this.rotuer.navigate(['/owner_signin']);
+        }
         return this.authService.isOwner();
     }
 }
