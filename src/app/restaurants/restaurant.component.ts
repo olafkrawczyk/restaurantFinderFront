53048aa5d97a62cfa7dad8b8d255aaba6604b368
Olaf Krawczyk <radeko94@gmail.com>
2017-11-02 21:49:30 +0100

    set date and other params querysting

--- src/app/restaurants/restaurant.component.ts
+++ src/app/restaurants/restaurant.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -8,8 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantComponent implements OnInit {
 
+    date: any;
+    people: any;
+    time: any;
+
+    constructor( private activatedRout: ActivatedRoute) {
+        this.date = new Date();
+    }
 
     ngOnInit(): void {
-        throw new Error('Method not implemented.');
+        this.activatedRout.params.subscribe(
+            (params: Params) => {
+                console.log('params');
+                console.log(params);
+            }
+        );
+        this.activatedRout.queryParams.subscribe(
+            (params: Params) => {
+                console.log('query params');
+                this.time = params['time'];
+                this.people = params['people'];
+                this.date = new Date(params['date']);
+                console.log(params);
+            }
+        );
     }
 }
