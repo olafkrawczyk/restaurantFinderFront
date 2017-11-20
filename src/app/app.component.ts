3c666047f61c2b615e9297ef3b698ee7a4636d1a
Olaf Krawczyk <radeko94@gmail.com>
2017-11-20 21:31:08 +0100

    typo fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { AuthService } from './auth/auth.service';
 import { Component } from '@angular/core';
 import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
+export const baseURL = 'http://localhost:8080';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [NgbDropdownConfig]
 })
-
-export const baseURL = 'http://localhost:8080';
-
 export class AppComponent {
   title = 'TableReserv';
 
